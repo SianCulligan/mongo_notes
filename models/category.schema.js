011f0fd1d0dc9523b3d890aaed14af3590c9cbfd
Sian Culligan <SianCulligan@gmail.com>
2020-03-22 20:35:49 -0700

    testing mongo

---
+++ models/category.schema.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema ({
+  name: { type: String,  required: true },
+});
+
+const categoryModel = mongoose.model('category', categorySchema);
+
+module.exports = categoryModel;
